ae13fb0e6a44208504cb703684779a42d5409b58
Goretti <Goretti@Goretti>
2015-10-01 10:23:20 +0100

    Kata2 Ultima version todo funciona

--- src/kata2_practicalab/Kata2_PracticaLab.java
+++ src/kata2_practicalab/Kata2_PracticaLab.java
@@ -4,14 +4,12 @@ package kata2_practicalab;
 public class Kata2_PracticaLab {
 
     public static void main(String[] args) {
-        String [] v11 = {"Ana","Pablo", "Juan", "Ana", "Ana","Juan"};
+        String [] v1 = {"Ana","Pablo", "Juan", "Ana", "Ana","Juan"};
         Integer [] v = {1,4,7,1,1,8,4,1,4,9};
         Histogram<Integer> histo = CalculaHistogram.computeHisto(v);
         
         
-        
-        
-        for (Integer key : histo.keySet()) {
+        for (Object key : histo.keySet()) {
             System.out.println(key+" : " + histo.get(key) + " veces");
         }
     }
