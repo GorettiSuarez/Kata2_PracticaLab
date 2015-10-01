bdd2a3fc2ca15b603fb4ef872439f8100a7cccf4
Goretti <Goretti@Goretti>
2015-10-01 09:20:29 +0100

    Kata 2 second version --> Profesor

---
+++ src/kata2_practicalab/Histogram.java
@@ -0,0 +1,26 @@
+package kata2_practicalab;
+
+import java.util.HashMap;
+
+public class Histogram {
+
+    private final int [] elVector;
+
+    public Histogram(int[] elVector) {
+        this.elVector = elVector;
+    }
+    
+    public HashMap<Integer,Integer> getHisto(){
+        HashMap <Integer,Integer> histo = new HashMap<>();
+        
+        for (int i = 0; i < elVector.length; i++) {
+            if(!histo.containsKey(elVector[i])){
+                histo.put(elVector[i], 1);
+            } else {
+                histo.put(elVector[i], histo.get(elVector[i])+1);
+            }
+        }
+        
+        return histo;
+    }
+}
