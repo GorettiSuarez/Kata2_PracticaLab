8a3d18f1fd36a28fec388845f0ecbeadf7bdbd5b
Goretti <Goretti@Goretti>
2015-10-01 09:20:02 +0100

    Kata 2 second version --> Profesor

--- src/kata2_practicalab/Kata2_PracticaLab.java
+++ src/kata2_practicalab/Kata2_PracticaLab.java
@@ -1,24 +1,18 @@
 package kata2_practicalab;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class Kata2_PracticaLab {
 
     public static void main(String[] args) {
         int [] v = {1,4,7,1,1,8,4,1,4,9};
-        Map<Integer,Integer> histogram = new HashMap<>();
+        Histogram histo = new Histogram(v);
+        Map<Integer,Integer> histogram = histo.getHisto();
         
-        for (int i = 0; i < v.length; i++) {
-            if(histogram.containsKey(v[i])){
-                histogram.put(v[i], histogram.get(v[i])+1);
-            } else {
-                histogram.put(v[i],1);
-            }
-        }
+        
         
         for (Integer key : histogram.keySet()) {
-            System.out.println(key+" : " + histogram.get(key));
+            System.out.println(key+" : " + histogram.get(key) + " veces");
         }
     }
     
